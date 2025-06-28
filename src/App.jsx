22f381805b0b1f0872f15c60f863ddab1bfd1ae3
Dulmika <semaldulmika@gmail.com>
2025-06-28 16:57:26 +0530

    create the responsive Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-center">
-      Hello world!
-    </h1>
+    <main className="bg-black">
+      <Navbar />
+    </main>
   )
 }
 
