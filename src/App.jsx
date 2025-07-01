ac0bb2c6115d11e5616c4989a73e410c1c5a82d2
Dulmika <semaldulmika@gmail.com>
2025-07-01 14:51:15 +0530

    create the Highlights component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Highlight from './components/Highlights';
 
 const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
+      <Highlight />
     </main>
   )
 }
